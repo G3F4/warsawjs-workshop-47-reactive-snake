f6327d994bdf8ecf59c5b6acea8a575805c7294c
G3F4 <kamil.grabek@gmail.com>
2020-08-22 20:19:43 +0200

    Updates

---
+++ precache-manifest.6d55c897e826c8e93b9149bb1e63eefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ae4d7310f6bcc054a0b9806e8a83c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d37ad71cb646abd794ae",
+    "url": "/static/css/main.637632f9.chunk.css"
+  },
+  {
+    "revision": "7e3ada53b39bd149e2fa",
+    "url": "/static/js/2.321a9fdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.321a9fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37ad71cb646abd794ae",
+    "url": "/static/js/main.e5e1fa07.chunk.js"
+  },
+  {
+    "revision": "6b4b039626c29faf6f2d",
+    "url": "/static/js/runtime-main.5c50a37b.js"
+  }
+]);
